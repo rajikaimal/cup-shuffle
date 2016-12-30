b29a51c6622164a37dc5aefdd6c10bc6ff6d1d3a
rajikaimal <inbox.rajika@gmail.com>
2016-12-30 23:55:56 +0530

    add structure

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="header">
+          <span className="title">Cup shuffle</span>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="game-area">
+          
+        </div>
       </div>
     );
   }
